b40f2b363a1fd1870d6c465902ceef5d68daa8ef
Frank Morton <fmorton@mac.com>
2022-11-01 10:46:57 -0400

    fix extlinks in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -25,8 +25,8 @@ version = release = '0.0.9'
 pygments_style = 'trac'
 templates_path = ['.']
 extlinks = {
-    'issue': ('https://github.com/fmorton/BirdBrain-Python-Library/issues/%s', '#'),
-    'pr': ('https://github.com/fmorton/BirdBrain-Python-Library/pull/%s', 'PR #'),
+    'issue': ('https://github.com/fmorton/BirdBrain-Python-Library/issues/%s', 'issue #%s'),
+    'pr': ('https://github.com/fmorton/BirdBrain-Python-Library/pull/%s', 'PR #%s'),
 }
 # on_rtd is whether we are on readthedocs.org
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
